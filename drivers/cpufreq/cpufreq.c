13fb84edcb25b5d161e49268780e8f3e0c74053b
Steve Garon <steve.garon@gmail.com>
2011-10-22 09:01:16 -0400

    Add 1320Mhz overclock with 220 FSB. But not on by default. Just in case more power is needed.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,7 +33,7 @@
 						"cpufreq-core", msg)
 
 /* UV */
-int exp_UV_mV[5] = { 1225000, 1175000, 1025000, 925000, 925000 };
+int exp_UV_mV[6] = { 1375000, 1225000, 1175000, 1025000, 925000, 925000 };
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
@@ -646,7 +646,7 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 /* sysfs interface for UV control */
 static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf) {
 
-	return sprintf(buf, "1100mhz: %d mV\n800mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n", exp_UV_mV[0]/1000, exp_UV_mV[1]/1000, exp_UV_mV[2]/1000, exp_UV_mV[3]/1000, exp_UV_mV[4]/1000);
+	return sprintf(buf, "1320mhz: %d mV\n1100mhz: %d mV\n800mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n", exp_UV_mV[0]/1000, exp_UV_mV[1]/1000, exp_UV_mV[2]/1000, exp_UV_mV[3]/1000, exp_UV_mV[4]/1000, exp_UV_mV[5]/1000);
 
 }
 
@@ -656,12 +656,12 @@ static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
 	unsigned int ret = -EINVAL;
 	int i = 0;
 
-	ret = sscanf(buf, "%d %d %d %d %d", &exp_UV_mV[0], &exp_UV_mV[1], &exp_UV_mV[2], &exp_UV_mV[3], &exp_UV_mV[4]);
-	if(ret != 5) {
+	ret = sscanf(buf, "%d %d %d %d %d %d", &exp_UV_mV[0], &exp_UV_mV[1], &exp_UV_mV[2], &exp_UV_mV[3], &exp_UV_mV[4], &exp_UV_mV[5]);
+	if(ret != 6) {
 		return -EINVAL;
 	}
 	else
-		for( i = 0; i < 5; i++ )
+		for( i = 0; i < 6; i++ )
 		{
 		   exp_UV_mV[i] *= 1000;
 		}
