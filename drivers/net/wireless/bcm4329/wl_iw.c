dbb685bc7630dbebabdf403864eb6b2601ded2bf
Steve Garon <steve.garon@gmail.com>
2011-11-22 18:34:40 -0500

    fixed merge error with wireless

--- drivers/net/wireless/bcm4329/wl_iw.c
+++ drivers/net/wireless/bcm4329/wl_iw.c
@@ -647,35 +647,6 @@ wl_iw_get_macaddr(
 	return error;
 }
 
-<<<<<<< HEAD
-static int 
-wl_iw_set_country_code(struct net_device *dev, char *ccode) 
-{ 
-  char country_code[WLC_CNTRY_BUF_SZ]; 
-  int ret = -1; 
- 
-  WL_TRACE(("%s\n", __FUNCTION__)); 
-  if (!ccode) 
-   ccode = dhd_bus_country_get(dev); 
-  strncpy(country_code, ccode, sizeof(country_code)); 
-  if (ccode && (country_code[0] != 0)) { 
-#ifdef CONFIG_US_NON_DFS_CHANNELS_ONLY 
-    if (use_non_dfs_channels && !strncmp(country_code, "US", 2)) 
-      strncpy(country_code, "Q2", WLC_CNTRY_BUF_SZ); 
-    if (!use_non_dfs_channels && !strncmp(country_code, "Q2", 2)) 
-      strncpy(country_code, "US", WLC_CNTRY_BUF_SZ); 
-#endif 
-    ret = dev_wlc_ioctl(dev, WLC_SET_COUNTRY, &country_code, sizeof(country_code)); 
-    if (ret >= 0) { 
-      WL_TRACE(("%s: set country %s OK\n", __FUNCTION__, country_code)); 
-      dhd_bus_country_set(dev, &country_code[0]); 
-    } 
-  } 
-  return ret; 
-} 
-=======
->>>>>>> a418278... Backport bcm4329 from tuna.
-
 static int
 wl_iw_set_country(
 	struct net_device *dev,
@@ -719,12 +690,9 @@ wl_iw_set_country(
 		}
 	}
 
-<<<<<<< HEAD
-	WL_ERROR(("%s: set country %s failed code %d\n", __FUNCTION__, country_code, error));
-=======
+
 	WL_ERROR(("%s: set country for %s as %s rev %d failed\n", \
 			__FUNCTION__, country_code, cspec.ccode, cspec.rev));
->>>>>>> a418278... Backport bcm4329 from tuna.
 
 	p += snprintf(p, MAX_WX_STRING, "FAIL");
 
@@ -798,7 +766,6 @@ bool btcoex_is_sco_active(struct net_device *dev)
 	for (i = 0; i < 12; i++) {
 
 		ioc_res = dev_wlc_intvar_get_reg(dev, "btc_params", 27, &param27);
-<<<<<<< HEAD
 
 		WL_TRACE_COEX(("%s, sample[%d], btc params: 27:%x\n",
 			__FUNCTION__, i, param27));
@@ -812,21 +779,6 @@ bool btcoex_is_sco_active(struct net_device *dev)
 			sco_id_cnt++;
 		}
 
-=======
-
-		WL_TRACE_COEX(("%s, sample[%d], btc params: 27:%x\n",
-			__FUNCTION__, i, param27));
-
-		if (ioc_res < 0) {
-			WL_ERROR(("%s ioc read btc params error\n", __FUNCTION__));
-			break;
-		}
-
-		if ((param27 & 0x6) == 2) {
-			sco_id_cnt++;
-		}
-
->>>>>>> a418278... Backport bcm4329 from tuna.
 		if (sco_id_cnt > 2) {
 			WL_TRACE_COEX(("%s, sco/esco detected, pkt id_cnt:%d  samples:%d\n",
 				__FUNCTION__, sco_id_cnt, i));
